bb7f094f58cddc92b3b1c3576e2d78434739e32a
Valeriy Osipov <dkutre@noking007@mai.ru>
2020-03-24 18:50:00 +0300

    Step 2

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 #include <cmath>
 #include <algorithm>
 
+#include <random>
+#include <cmath>
+
 double H = 2000.0;
 double V = 100.0;
 double am = 20000.0;
@@ -15,7 +18,8 @@ double P1 =  -0.15;
 
 double hkm = H / 1000;
 double ro = 1.22235514 - 0.11422776 * hkm + 0.00368329 * hkm * hkm - 0.0000404 * hkm * hkm * hkm;
-double Xaer = 0.8 * ro * V * V * S / 2.0;
+double cx = 0.8;
+double Xaer = cx* ro * V * V * S / 2.0;
 
 double P2 = P1 * V / (g - Xaer / am);
 double P3 = -0.7;
@@ -50,13 +54,13 @@ void FUN2 (int dimension, double x0, vector<double> y, vector<double>& dy, int& 
 
     hkm = y[0] / 1000;
     ro = 1.22235514 - 0.11422776 * hkm + 0.00368329 * hkm * hkm - 0.0000404 * hkm * hkm * hkm;
-    Xaer = 0.8 * ro * y[1] * y[1] * S / 2.0;
+    Xaer = cx * ro * y[1] * y[1] * S / 2.0;
 
 
-    double drodH = (-0.11422776 + 2.0 * 0.00368329 * hkm - 3.0 * 0.0000404 * hkm * hkm) * 0.001;
     dy[0] = -y[1] * T ;
     dy[1] = (-(beta * U + Xaer) / y[2] + g) * T;
     dy[2] = - U * T;
+    double drodH = (-0.11422776 + 2.0 * 0.00368329 * hkm - 3.0 * 0.0000404 * hkm * hkm) * 0.001;
     dy[3] = (y[4] * 0.8 * y[1] * y[1] * S * drodH * T) / (2.0 * y[2]);
     dy[4] = (y[3] + y[4] * Xaer * 2.0 / y[1] / y[2]) * T;
     dy[5] = -y[4] * (beta * U + Xaer) * T / (y[2] * y[2]);
@@ -107,7 +111,7 @@ void getResidual (const vector<double> & x, vector<double>& residual)
 
     hkm = H / 1000;
     ro = 1.22235514- 0.11422776 * hkm + 0.00368329 * hkm * hkm - 0.0000404 * hkm * hkm * hkm;
-    Xaer = 0.8 * ro * V * V * S / 2.0;
+    Xaer = cx * ro * V * V * S / 2.0;
 
 
     double x0 = 0.0;
@@ -183,15 +187,6 @@ void setInitalValue()
     RESULT[0][11] = Xaer;
 }
 
-double findSwitchMoment()
-{
-    for (auto elem : RESULT) {
-        if (elem[8] > 0) {
-            return elem[10];
-        }
-    }
-}
-
 void massAnalyz()
 {
     Matrix correlation = Matrix(1000, vector<double>(3, 0)); // зависимость масса время посадки и расход топлива
@@ -211,7 +206,7 @@ void massAnalyz()
         newton_steps = 0;
         newtonClassic(&getResidual, 3, args, {1, 1, 1}, newton_steps);
         correlation[i][0] = RESULT[0][1]; // записываем массу
-        correlation[i][1] = findSwitchMoment(); // момент переключения
+    //    correlation[i][1] = findSwitchMoment(); // момент переключения
         correlation[i][2] = RESULT[0][3] - RESULT[1000][3]; // затраченное топливо
         if (i % 100 == 0) {
             std::cout << "ke" << std::endl;
@@ -222,64 +217,241 @@ void massAnalyz()
     printMatrix(correlation);
 }
 
+std::size_t findSwitchIndex(const Matrix & matrix)
+{
+    for (size_t i = 0; i < matrix.size(); i++)
+        if (matrix[i][9] > 0) // тяга стала больше 0
+            return i - 1;
+    throw std::runtime_error("нет момента переключения");
+}
 
-void FUN2_2 (int dimension, double x0, vector<double> y, vector<double>& dy, int& exit_signal, unsigned int step_number)
+
+void free_fall (int dimension, double x0, vector<double> y, vector<double>& dy, int& exit_signal, unsigned int step_number)
 {
     hkm = y[0] / 1000;
-    ro = 1.22235514 - 0.11422776 * hkm + 0.00368329 * hkm * hkm - 0.0000504 * hkm * hkm * hkm;
-    Xaer = 0.8 * ro * y[1] * y[1] * S / 2.0;
-
+    ro = 1.22235514 - 0.11422776 * hkm + 0.00368329 * hkm * hkm - 0.0000404 * hkm * hkm * hkm;
+    Xaer = cx * ro * y[1] * y[1] * S / 2.0;
+    /*
     double a = -0.8 * ro * S / 1.0 / y[2];
     double b = -beta / y[2];
     double c = a * y[1];
-
-//    std::cout << y[0] << "," << y[1] << "," << y[2] << " ";
-//    std::cout << a << "_" << b << "_" << c << std::endl;
 
     double P12 = 1.0 / b;
     double P22 = 2.0 * c / b / b + sqrt((4.0 * c * c + b * b - 2.0 * P12 * b * b) / b / b / b / b);
     double dx1 = INTERMEDIATE_RESULT[step_number][1] - OPTIMAL[step_number][1];
     double dx2 = INTERMEDIATE_RESULT[step_number][2] - OPTIMAL[step_number][2];
     double dU = -b * (P12 * dx1 + P22 * dx2);
-    U = OPTIMAL[step_number + 1][9] + dU; // т.к методе Ньютора когда достигаем момента переключения делаем шаг назад и заново просчитываем шаг уже с учетом U_switch
+    U = OPTIMAL[step_number + 1][9] + dU;
     U = std::max(0.0, std::min(U, 305.0));
+    т.к методе Ньютора когда достигаем момента переключения делаем шаг назад и заново просчитываем шаг уже с учетом U_switch
 
     RESULT[step_number][dimension + 4] = dU;// ! запись дополнительных переменных
     RESULT[step_number][dimension + 5] = dx1;// ! запись дополнительных переменых
     RESULT[step_number][dimension + 6] = dx2;// ! запись дополнительных переменых
     RESULT[step_number][dimension + 7] = U;//y[3];// ! запись дополнительных переменных
+    */
     //std::cout <<step_number << " " << -b << " " << " " << P12 << " " << P22 << " " << dx1 << " " << dx2 <<  " " << dU * T << std::endl;
 
+    U = 0;
     dy[0] = -y[1] * T;
     dy[1] = (-(beta * U + Xaer) / y[2] + g) * T;
     dy[2] = - U * T;
 
-    if (fabs(1.0 / rungeKuttaSteps * (step_number + rkStepsFirst) - x0) < 1e-10) {
-        tabs = x0 * T;
-        //RESULT[rkStepsFirst + step_number][dimension + 1] = y[3];// ! запись дополнительных переменных
-        RESULT[rkStepsFirst + step_number][dimension + 2] = tabs;// ! запись дополнительных переменных
-        RESULT[rkStepsFirst + step_number][dimension + 3] = Xaer;// ! запись дополнительных переменных
-        if (step_number >= rkStepsSecond && rkStepsSecond != 0) {
-            exit_signal = 1;
-        }
+    static auto local_step_number = step_number;
+    if (local_step_number == step_number)
+        return;
+    //std::cout << step_number;
+    local_step_number = step_number;
+    auto switch_index = findSwitchIndex(OPTIMAL);
+    if (y[1] > OPTIMAL[switch_index][2] || y[0] < OPTIMAL[switch_index][1])
+    {
+        //std::cout << "\nexit_signal = 1, step" << step_number << std::endl;
+        //std::cout << y[1] << " " << OPTIMAL[switch_index][2] << " " << y[0] << " " << OPTIMAL[switch_index][1];
+        exit_signal = 1;
     }
 }
 
-void execRK (const vector<double> & x)
+double linear_interpolation(double x, double x0, double y0, double x1, double y1)
 {
-    std::cout << "execRK()" << std::endl;
+    if (x1 == x0)
+        throw std::runtime_error("Can't do linear interpolation, x0 == x1");
+    //std::cout << "x: " << x << ", x0: " << x0 << ", y0: " << y0 << ", x1:" << x1 << ", y1:" << y1 << std::endl;
+    const double y = y0 + ((y1 - y0) / (x1 - x0)) * (x - x0);
+    //std::cout << "y: " << y << std::endl;
+    return y;
+}
+
+double getVByH(const Matrix& stats, double H)
+{
+    for (size_t i = 1; i < stats.size(); i++) // должна быть точка слева
+    {
+        if (stats[i][1] < H)
+        {
+           const std::size_t left = i - 1;
+           const std::size_t right = i;
+           double res = linear_interpolation(H, stats[left][1], stats[left][2], stats[right][1], stats[right][2]);
+           return res;
+        }
+    }
+    return 0;
+}
+
+void engine_on (int dimension, double x0, vector<double> y, vector<double>& dy, int& exit_signal, unsigned int step_number)
+{
+    hkm = y[0] / 1000;
+    ro = 1.22235514 - 0.11422776 * hkm + 0.00368329 * hkm * hkm - 0.0000404 * hkm * hkm * hkm;
+    Xaer = cx * ro * y[1] * y[1] * S / 2.0;
+    /*
+    double a = -0.8 * ro * S / 1.0 / y[2];
+    double b = -beta / y[2];
+    double c = a * y[1];
+
+    double P12 = 1.0 / b;
+    double P22 = 2.0 * c / b / b + sqrt((4.0 * c * c + b * b - 2.0 * P12 * b * b) / b / b / b / b);
+    double dx1 = INTERMEDIATE_RESULT[step_number][1] - OPTIMAL[step_number][1];
+    double dx2 = INTERMEDIATE_RESULT[step_number][2] - OPTIMAL[step_number][2];
+    double dU = -b * (P12 * dx1 + P22 * dx2);
+    U = OPTIMAL[step_number + 1][9] + dU;
+    U = std::max(0.0, std::min(U, 305.0));
+    т.к методе Ньютора когда достигаем момента переключения делаем шаг назад и заново просчитываем шаг уже с учетом U_switch
+
+//   RESULT[step_number][dimension + 4] = dU;// ! запись дополнительных переменных
+//    RESULT[step_number][dimension + 5] = dx1;// ! запись дополнительных переменых
+//    RESULT[step_number][dimension + 6] = dx2;// ! запись дополнительных переменых
+//    RESULT[step_number][dimension + 7] = U;//y[3];// ! запись дополнительных переменных
+//    */
+    //std::cout <<step_number << " " << -b << " " << " " << P12 << " " << P22 << " " << dx1 << " " << dx2 <<  " " << dU * T << std::endl;
+
+    static auto local_step_number = -1;
+    static double akp = 25;
+    static double delta_v = 0;
+    static double delta_u = akp * delta_v;
+    //std::cout << "y[1]: " << y[1] << ", step: " << step_number << std::endl;
+    if (local_step_number != step_number)
+    {
+        delta_v = y[1] - getVByH(OPTIMAL, y[0]);
+        delta_u = akp * delta_v;
+    //    std::cout << "speed: " << y[1] << " " << getVByH(OPTIMAL, y[0]) << "\n";
+    //    std::cout << "delta_v: " << delta_v << std::endl;
+    //    std::cout << "delta_u: " << delta_u << std::endl;
+        local_step_number = step_number;
+    }
+
+    U = u_max + delta_u;
+    dy[0] = -y[1] * T;
+    dy[1] = (-(beta * U + Xaer) / y[2] + g) * T;
+    dy[2] = - U * T;
+
+    if (y[1] < OPTIMAL.back()[2] || y[0] < OPTIMAL.back()[1])
+    {
+    //    std::cout << "\nexit_signal = 1, step" << step_number << std::endl;
+        exit_signal = 1;
+    }
+    //std::cout << "=======" << std::endl;
+}
+
+void end_free_fall (int dimension, double x0, vector<double> y, vector<double>& dy, int& exit_signal, unsigned int step_number)
+{
+    hkm = y[0] / 1000;
+    ro = 1.22235514 - 0.11422776 * hkm + 0.00368329 * hkm * hkm - 0.0000404 * hkm * hkm * hkm;
+    Xaer = cx * ro * y[1] * y[1] * S / 2.0;
+    /*
+    double a = -0.8 * ro * S / 1.0 / y[2];
+    double b = -beta / y[2];
+    double c = a * y[1];
+
+    double P12 = 1.0 / b;
+    double P22 = 2.0 * c / b / b + sqrt((4.0 * c * c + b * b - 2.0 * P12 * b * b) / b / b / b / b);
+    double dx1 = INTERMEDIATE_RESULT[step_number][1] - OPTIMAL[step_number][1];
+    double dx2 = INTERMEDIATE_RESULT[step_number][2] - OPTIMAL[step_number][2];
+    double dU = -b * (P12 * dx1 + P22 * dx2);
+    U = OPTIMAL[step_number + 1][9] + dU;
+    U = std::max(0.0, std::min(U, 305.0));
+    т.к методе Ньютора когда достигаем момента переключения делаем шаг назад и заново просчитываем шаг уже с учетом U_switch
+
+    RESULT[step_number][dimension + 4] = dU;// ! запись дополнительных переменных
+    RESULT[step_number][dimension + 5] = dx1;// ! запись дополнительных переменых
+    RESULT[step_number][dimension + 6] = dx2;// ! запись дополнительных переменых
+    RESULT[step_number][dimension + 7] = U;//y[3];// ! запись дополнительных переменных
+    */
+    //std::cout <<step_number << " " << -b << " " << " " << P12 << " " << P22 << " " << dx1 << " " << dx2 <<  " " << dU * T << std::endl;
+
+    U = 0;
+    dy[0] = -y[1] * T;
+    dy[1] = (-(beta * U + Xaer) / y[2] + g) * T;
+    dy[2] = - U * T;
+
+    static auto local_step_number = step_number;
+    if (local_step_number == step_number)
+        return;
+    //std::cout << step_number;
+    local_step_number = step_number;
+    if (y[0] < 0)
+    {
+        std::cout << "\nexit_signal = 1, step" << step_number << std::endl;
+        exit_signal = 1;
+    }
+}
+
+void append(const Matrix& source, const size_t source_size, Matrix& dest)
+{
+    for (size_t i = 0; i <= source_size && i < source.size(); i++)
+        dest.push_back(source[i]);
+}
+
+void appendWithoutFirstRow(const Matrix& source, const size_t source_size, Matrix& dest)
+{
+    for (size_t i = 1; i <= source_size && i < source.size(); i++)
+        dest.push_back(source[i]);
+}
+
+// для последней строчки считаем значения при 0 высоте
+void calcLastRow(Matrix& matrix)
+{
+    //std::cout << "calcLastRow()" << std::endl;
+    if (matrix.size() < 2)
+        throw std::runtime_error("Should be more then one row");
+    const auto second_from_end_row = matrix[matrix.size() - 2]; // вторая с конца строка
+    //std::cout << "second_from_end_row";
+    printContainer(second_from_end_row);
+    const auto last_row = matrix.back();
+    //std::cout << "last_row";
+    printContainer(last_row);
+
+    auto new_last_row = last_row;
+    for (std::size_t i = 2; i < last_row.size(); i++)
+    {
+       if (second_from_end_row[i] != last_row[i])
+           new_last_row[i] = linear_interpolation(
+               0,
+               second_from_end_row[1],
+               second_from_end_row[i],
+               last_row[1],
+               last_row[i]
+               );
+    }
+    new_last_row[1] = 0;
+    //std::cout << "new_last_row";
+    printContainer(new_last_row);
+
+    matrix.back() = new_last_row;
+}
+
+void execRK (const vector<double> & x, const vector<double> init_y, Matrix & all_result)
+{
+    //std::cout << "execRK()" << std::endl;
     resetGlobalVariables();
 
     int dimension = 3;
     rkStepsFirst  = 0; // N2
     rkStepsSecond = 0; // N3
 
-    vector<double> y(3, 0);
+    vector<double> y = init_y;
     int exit_signal;
 
-    hkm = H / 1000;
-    ro = 1.32235514- 0.11422776 * hkm + 0.00368329 * hkm * hkm - 0.0000404 * hkm * hkm * hkm;
-    Xaer = 0.8 * ro * V * V * S / 2.0;
+    hkm = y[0] / 1000;
+    ro = 1.32235514 - 0.11422776 * hkm + 0.00368329 * hkm * hkm - 0.0000404 * hkm * hkm * hkm;
+    Xaer = cx * ro * y[1] * y[1] * S / 2.0;
     U = 0;
 
     double x0 = 0.0;
@@ -291,22 +463,139 @@ void execRK (const vector<double> & x)
     //! if (XX(3).Lt.5.d0) XX(3)=5.d0
     T = x[2];
 
-    y[0] = H;
-    y[1] = V;
-    y[2] = am;
+    //y[0] = H;
+    //y[1] = V;
+    //y[2] = am;
 
     int temp_step = 0;
-    std::cout << "RK4()" << std::endl;
-    RK4(FUN2_2, dimension, rungeKuttaSteps, x0, dx, y, exit_signal, temp_step, INTERMEDIATE_RESULT);
+    std::cout << "RK4(), free_fall" << std::endl;
+    RK4(free_fall, dimension, rungeKuttaSteps, x0, dx, y, exit_signal, temp_step, INTERMEDIATE_RESULT);
     std::cout << "/RK4()" << std::endl;
-    rkStepsFirst = temp_step;
+    std::cout << temp_step;
+    append(INTERMEDIATE_RESULT, temp_step, all_result);
 
-    for (int i = 0; i < rkStepsFirst + 1; i++) {
-        for (int j = 0; j < dimension + 1; j++) {
-            RESULT[i][j] = INTERMEDIATE_RESULT[i][j];
-        }
+    INTERMEDIATE_RESULT = Matrix(rungeKuttaSteps + 1, vector<double>(7, 0));
+    std::cout << "RK4(), engine_on" << std::endl;
+    y[0] = all_result.back()[1];
+    y[1] = all_result.back()[2];
+    y[2] = all_result.back()[3];
+    RK4(engine_on, dimension, rungeKuttaSteps, x0, dx, y, exit_signal, temp_step, INTERMEDIATE_RESULT);
+    std::cout << "/RK4()" << std::endl;
+    appendWithoutFirstRow(INTERMEDIATE_RESULT, temp_step, all_result);
+
+    // недолетели, т.е отрицательная скорость на ненулевой высоте (летим вверх)
+    if (all_result.back()[1] > OPTIMAL.back()[1])
+    {
+        INTERMEDIATE_RESULT = Matrix(rungeKuttaSteps + 1, vector<double>(7, 0));
+        std::cout << "RK4(), free fall after engine off" << std::endl;
+        y[0] = all_result.back()[1];
+        y[1] = all_result.back()[2];
+        y[2] = all_result.back()[3];
+        RK4(end_free_fall, dimension, rungeKuttaSteps, x0, dx, y, exit_signal, temp_step, INTERMEDIATE_RESULT);
+        std::cout << "/RK4()" << std::endl;
+        appendWithoutFirstRow(INTERMEDIATE_RESULT, temp_step, all_result);
     }
+
+    calcLastRow(all_result); // высчитать значения при нулевой высоте
+
+    RESULT = all_result;
     std::cout << "/execRK()" << std::endl;
+}
+
+Matrix h_distribution()
+{
+    vector<double> args = {P1, P3, T};
+
+    double h_variance = H * 0.03;
+    std::random_device h_rd{};
+    std::mt19937 h_gen{h_rd()};
+    std::normal_distribution<> dh{H, h_variance};
+
+    Matrix results_with_random(0, vector<double>(12, 0));
+    for (std::size_t i = 0; i < 1000; i++)
+    {
+        Matrix result(0, vector<double>(12, 0));
+        H = dh(h_gen);
+        vector<double> init_y = {H, V, am};
+        execRK(args, init_y, result);
+        if (result.back()[2] < 0)
+            continue;
+        results_with_random.push_back(result.back());
+        results_with_random.back().back() = H;
+    }
+    return results_with_random;
+}
+
+Matrix v_distribution()
+{
+    vector<double> args = {P1, P3, T};
+
+    double variance = V * 0.05;
+    std::random_device rd{};
+    std::mt19937 gen{rd()};
+    std::normal_distribution<> d{V, variance};
+
+    Matrix results_with_random(0, vector<double>(12, 0));
+    for (std::size_t i = 0; i < 10000; i++)
+    {
+        Matrix result(0, vector<double>(12, 0));
+        double local_V = d(gen);
+        vector<double> init_y = {H, local_V, am};
+        execRK(args, init_y, result);
+        if (result.back()[2] < 0)
+            continue;
+        results_with_random.push_back(result.back());
+        results_with_random.back().back() = local_V;
+    }
+    return results_with_random;
+}
+
+Matrix m_distribution()
+{
+    vector<double> args = {P1, P3, T};
+
+    double variance = am * 0.01;
+    std::random_device rd{};
+    std::mt19937 gen{rd()};
+    std::normal_distribution<> d{am, variance};
+
+    Matrix results_with_random(0, vector<double>(12, 0));
+    for (std::size_t i = 0; i < 1000; i++)
+    {
+        Matrix result(0, vector<double>(12, 0));
+        double local_am = d(gen);
+        vector<double> init_y = {H, V, local_am};
+        execRK(args, init_y, result);
+        if (result.back()[2] < 0)
+            continue;
+        results_with_random.push_back(result.back());
+        results_with_random.back().back() = local_am;
+    }
+    return results_with_random;
+}
+
+Matrix cx_distribution()
+{
+    vector<double> args = {P1, P3, T};
+
+    double variance = cx * 0.03;
+    std::random_device rd{};
+    std::mt19937 gen{rd()};
+    std::normal_distribution<> d{cx, variance};
+
+    Matrix results_with_random(0, vector<double>(12, 0));
+    for (std::size_t i = 0; i < 10000; i++)
+    {
+        Matrix result(0, vector<double>(12, 0));
+        cx = d(gen);
+        vector<double> init_y = {H, V, am};
+        execRK(args, init_y, result);
+        if (result.back()[2] < 0)
+            continue;
+        results_with_random.push_back(result.back());
+        results_with_random.back().back() = cx;
+    }
+    return results_with_random;
 }
 
 void synthesis()
@@ -319,8 +608,17 @@ void synthesis()
     newtonClassic(&getResidual, 3, args, residuals, newton_steps);
     std::cout << "/newtonClassic()" << std::endl;
     OPTIMAL = RESULT;
-    execRK(args);
-    printMatrix(RESULT);
+
+
+    //Matrix result(0, vector<double>(12, 0));
+    //vector<double> init_y = {H, 1998.422, am};
+    //execRK(args, init_y, result);
+    //printMatrix(result);
+    //const auto results_to_print = h_distribution();
+    //const auto results_to_print = v_distribution();
+    //const auto results_to_print = m_distribution();
+    const auto results_to_print = cx_distribution();
+    printMatrix(results_to_print);
     //printMatrix(OPTIMAL);
     std::cout << "/synthesis()" << std::endl;
 }
